517a9b0641eb227d8c1a0b487854618dc9504246
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-10 18:33:49 +0600

    fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,28 +55,9 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
         heading: ["var(--font-heading)", ...fontFamily.sans],
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
@@ -84,16 +65,6 @@ module.exports = {
     require("tailwindcss-animate"),
     nextui({
       defaultTheme: "dark",
-      themes: {
-        dark: {
-          colors: {
-            primary: {
-              DEFAULT: "hsl(var(--primary))",
-              foreground: "hsl(var(--primary-foreground))",
-            },
-          },
-        },
-      },
     }),
   ],
 };
