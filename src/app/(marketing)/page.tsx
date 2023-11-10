96262f7962668d9196b8b0928dec947f9f0119dc
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-10 19:19:51 +0600

    deployment test

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/header/hero-section"
-// import SubmissionForm from "@/components/submission/submission-form"
+import SubmissionForm from "@/components/submission/submission-form"
 import { GptCollection } from "@/components/gpt/gpt-collection"
 import FAQ from "@/components/footer/faq"
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="my-6 mx-2 lg:mx-0">
       <HeroSection />
-      {/* <SubmissionForm /> */}
+      <SubmissionForm />
       <GptCollection />
       <FAQ />
     </main>
