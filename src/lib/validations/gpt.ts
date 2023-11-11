4ab5e3c9548d1d85107a6af4942beb9a002c7eaa
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-11 23:20:59 +0600

    new schema

--- src/lib/validations/gpt.ts
+++ src/lib/validations/gpt.ts
@@ -8,4 +8,19 @@ export const gptSchema = z.object({
     .refine((url) => url.includes("chat.openai.com/g/g-"), {
       message: "URL must include 'chat.openai.com/g/g-'",
     }),
+  tag: z.string().default('General'),
+});
+
+export const gptSchemaBody = z.object({
+  email: z.string().email(),
+  url: z
+    .string()
+    .url()
+    .refine((url) => url.includes("chat.openai.com/g/g-"), {
+      message: "URL must include 'chat.openai.com/g/g-'",
+    }),
+  tag: z.string().default('General'),
+  name: z.string().min(1),
+  description: z.string().min(1),
+  imageUrl: z.string().url(),
 });
