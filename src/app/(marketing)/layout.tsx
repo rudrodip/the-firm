5ea0f091be06469edf7ad26e37ac67d70b5da921
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-11 23:19:20 +0600

    removed unnecessary things

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,3 @@
-import { marketingConfig } from "@/config/marketing";
-import { MainNav } from "@/components/navbar/main-nav";
 import Link from "next/link";
 import { Icons } from "@/components/icons";
 import { siteConfig } from "@/config/site";
@@ -15,14 +13,13 @@ export default async function MarketingLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <header className="z-40 sticky top-0 backdrop-blur-md">
-        <div className="flex h-15 items-center mx-2 justify-between md:justify-evenly py-5">
-          <Link href="/" className="hidden items-center space-x-2 md:flex">
+        <div className="flex h-12 lg:h-15 items-center mx-2 justify-between md:justify-evenly py-5">
+          <Link href="/" className="flex items-center space-x-2">
             <Icons.logo />
             <span className="hidden font-bold sm:inline-block">
               {siteConfig.name}
             </span>
           </Link>
-          {/* <MainNav items={marketingConfig.mainNav} /> */}
           <a href={siteConfig.links.github} target="_blank">
             <Icons.gitHub className="w-8 h-8" />
           </a>
