47899035a49e58d0e5b175dbeda1ac5226eaf8bc
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-11 23:19:55 +0600

    refactor

--- src/components/gpt/gpt-collection.tsx
+++ src/components/gpt/gpt-collection.tsx
@@ -1,24 +1,17 @@
-import GptCard from "./gpt-card";
 import { GPT, PrismaClient } from "@prisma/client";
+import GptGrid from "./gpt-search";
 
 export async function GptCollection() {
   const prisma = new PrismaClient();
-  let gpts: GPT[] = []
+  let gpts: GPT[] = [];
   try {
-    gpts = await prisma.gPT.findMany()
+    gpts = await prisma.gPT.findMany();
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
   return (
     <>
-      <section
-        id="collection"
-        className="mt-10 lg:mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3"
-      >
-        {gpts.map((gpt, i) => {
-          return <GptCard key={i} gpt={gpt} />;
-        })}
-      </section>
+      <GptGrid gpts={gpts} />
     </>
   );
 }
