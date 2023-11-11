60743726f980d900dd69bd29d34b9516cb580c0a
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-11 23:20:06 +0600

    search functionality

--- src/components/gpt/gpt-search.tsx
+++ src/components/gpt/gpt-search.tsx
@@ -1,53 +1,74 @@
 "use client";
 
-import { Button, Input } from "@nextui-org/react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import { useForm, Controller } from "react-hook-form";
-import { gpt } from "@/types";
+import { useState } from "react";
+import { Input } from "@nextui-org/react";
+import GptCard from "./gpt-card";
+import { GPT } from "@prisma/client";
+import { cn } from "@/lib/utils";
 
-const FormSchema = z.object({
-  query: z.string().min(1, {
-    message: "Search query must be atleast 1 character long",
-  }),
-});
+export default function GptGrid({ gpts }: { gpts: GPT[] }) {
+  const [currentTag, setCurrentTag] = useState<string>("all");
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
-export default function GptGrid({ gpts }: { gpts: gpt[] }) {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors, isLoading },
-  } = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
+  const handleTagClick = (tag: string) => {
+    setCurrentTag(tag);
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredGpts = gpts.filter((gpt) => {
+    const isTagMatch = currentTag === "all" || currentTag === gpt.tag;
+    const isSearchMatch =
+      searchQuery.length === 0 ||
+      gpt.name.toLowerCase().includes(searchQuery.toLowerCase());
+    return isTagMatch && isSearchMatch;
   });
 
-  const onSubmit = (data: z.infer<typeof FormSchema>) => {
-    console.log(data)
-  };
   return (
     <section id="explore" className="flex flex-col my-24 items-center">
       <h1 className="font-heading text-5xl text-center">Explore GPTs</h1>
-      {/* <form onSubmit={handleSubmit(onSubmit)} className="w-full lg:w-1/2 mt-4">
-        <div className="flex w-full flex-wrap md:flex-nowrap gap-4 items-center">
-          <Controller
-            name="query"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <Input
-                isRequired
-                type="text"
-                label="Search"
-                errorMessage={errors.query && errors.query.message}
-                {...field}
-              />
-            )}
-          />
-          <Button type="submit" color="primary" size="lg" isLoading={isLoading}>
-            Search
-          </Button>
-        </div>
-      </form> */}
+      <Input
+        isRequired
+        type="text"
+        label="Search"
+        className="w-full lg:w-1/2 my-4"
+        onChange={handleInput}
+      />
+      <div className="flex flex-wrap gap-3 my-5 max-w-7xl justify-center items-center">
+        <span
+          className={cn(
+            "px-2 py-1 border backdrop-blur-md rounded-md cursor-pointer",
+            currentTag == "all" && "bg-primary"
+          )}
+          onClick={() => handleTagClick("all")}
+        >
+          All
+        </span>
+        {gpts.map((gpt, key) => {
+          return (
+            <span
+              key={key}
+              className={cn(
+                "px-2 py-1 border backdrop-blur-md rounded-md cursor-pointer",
+                currentTag == gpt.tag && "bg-primary"
+              )}
+              onClick={() => handleTagClick(gpt.tag)}
+            >
+              {gpt.tag}
+            </span>
+          );
+        })}
+      </div>
+      <section
+        id="collection"
+        className="mt-10 lg:mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3"
+      >
+        {filteredGpts.map((gpt) => {
+          return <GptCard key={gpt.id} gpt={gpt} />;
+        })}
+      </section>
     </section>
   );
 }
