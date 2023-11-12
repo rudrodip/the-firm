026297f74c88322faa9f6ec2ca495b59ac88616c
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-12 10:03:37 +0600

    added get gpts func with 600 sec cache

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { env } from "@env.mjs";
+import { cache } from 'react'
+import { PrismaClient } from "@prisma/client";
+
+export const revalidate = 600
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,3 +13,9 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   return `${env.NEXT_PUBLIC_APP_URL}${path}`;
 }
+
+export const getGpts = cache(async () => {
+  const prisma = new PrismaClient();
+  const gpts = await prisma.gPT.findMany();
+  return gpts
+})
