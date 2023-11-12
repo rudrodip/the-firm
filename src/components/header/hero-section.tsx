58588e56c81713e1b6267d3446b5ba6645574a7b
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-12 10:02:53 +0600

    style changed

--- src/components/header/hero-section.tsx
+++ src/components/header/hero-section.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
       <div className="px-3 py-1 rounded-full border border-primary/20 btn-gradient my-2">
         <a href="#explore">Find your GPT</a>
       </div>
-      <h1 className="head-text">Learn Better with <span className="orange-gradient">EduGPTs</span></h1>
+      <h1 className="head-text text-center">Learn Better with <span className="orange-gradient">EduGPTs</span></h1>
       <h1 className="desc">Largest Collection of GPTs for educational purposes</h1>
     </section>
   )
