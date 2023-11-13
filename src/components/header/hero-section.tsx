1fce4463e6aa1ac7a18bbbcdcef9e54fb6eb0801
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-13 22:13:11 +0600

    Update hero section with new AI workforce information

--- src/components/header/hero-section.tsx
+++ src/components/header/hero-section.tsx
@@ -11,19 +11,15 @@ const motionProps = {
 export default function HeroSection() {
   return (
     <motion.section className="flex flex-col items-center justify-center my-0 lg:my-20">
-      <motion.div
-        className="shine shine-anim px-4 py-1 rounded-full border border-primary/20 btn-gradient my-2"
-      >
-        <a href="#explore">Find your GPT</a>
+      <motion.div className="shine shine-anim px-4 py-1 rounded-full border border-primary/20 btn-gradient my-2">
+        <a href="#explore">Discover AI Workforce</a>
       </motion.div>
-      <motion.h1
-        className="head-text text-center"
-        {...motionProps}
-      >
-        Learn Better with <span className="orange-gradient">EduGPTs</span>
+      <motion.h1 className="head-text text-center" {...motionProps}>
+        Transform Your Business with{" "}
+        <span className="orange-gradient">TheFirm</span>
       </motion.h1>
       <motion.h1 className="text-lg text-center" {...motionProps}>
-        Largest Collection of GPTs for educational purposes
+        A Diverse Team of GPTs Ready for Any Task
       </motion.h1>
     </motion.section>
   );
